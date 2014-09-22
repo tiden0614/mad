b846a8c0274a5f502bc4fd931bdc8ca634a57051
cl <zhouqihang@gmail.com>
2014-09-22 23:02:53 +0930

    framework-trial

--- client/www/js/app.js
+++ client/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('farmers', ['ionic', 'farmers.controllers', 'farmers.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -30,64 +30,27 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       controller: 'AppCtrl'
     })
 
-    .state('app.search', {
-      url: "/search",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/search.html"
+    .state('app.alert', {
+        url:"/forecasts/alert",
+        views: {
+          'menuContent' :{
+            templateUrl: "templates/alert.html",
+            controller: 'AlertCtrl'
+          }
         }
-      }
     })
 
-    .state('app.browse', {
-      url: "/browse",
+    .state('app.forecasts', {
+      url: "/forecasts",
       views: {
         'menuContent' :{
-          templateUrl: "templates/browse.html"
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-    .state('app.single', {
-      url: "/playlists/:playlistId",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlist.html",
-          controller: 'PlaylistCtrl'
-        }
-      }
-    })
-
-    .state('app.forecastDetail', {
-      url: "/forecastList/:forecastId",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/forecast.html",
-          controller: 'ForecastDetailCtrl'
-        }
-      }
-    })
-
-    .state('app.forecastList', {
-      url: '/forecastList',
-      views: {
-        'menuContent' :{
-          templateUrl: 'templates/forecast-list.html',
-          controller: 'ForecastListCtrl'
+          templateUrl: "templates/forecasts.html",
+          controller: 'ForecastsCtrl'
         }
       }
     });
-  // if none of the above states are matched, use this as the fallback
-  //$urlRouterProvider.otherwise('/app/playlists');
-  $urlRouterProvider.otherwise('/app/forecastList');
-});
 
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/forecasts');
+
+});
