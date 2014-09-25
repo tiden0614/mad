f9930e741a157416e8b2bc3c6aecb31f4743b59b
Daryl Zhang <tiden111@gmail.com>
2014-09-25 22:08:15 +0930

    Client: Reformat some code

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -1,77 +1,77 @@
 angular.module('farmers.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $state, $timeout, LocationList) {
+  .controller('AppCtrl', function ($scope, $ionicModal, $state, $timeout, LocationList) {
 
-  $scope.search = function(){
-    $state.transitionTo('app.search');
-  }
+    $scope.search = function () {
+      $state.transitionTo('app.search');
+    }
 
-  $scope.locationList = LocationList.all();
+    $scope.locationList = LocationList.all();
 
-  // Form data for the login modal
-  $scope.loginData = {};
+    // Form data for the login modal
+    $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.modal = modal;
+    });
 
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+      $scope.modal.hide();
+    };
 
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
+    // Open the login modal
+    $scope.login = function () {
+      $scope.modal.show();
+    };
 
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+      console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function () {
+        $scope.closeLogin();
+      }, 1000);
+    };
 
-})
+  })
 
-.controller('ForecastsCtrl', function($scope, $state, ForecastList) {
-  $scope.forecastList = ForecastList.all();
+  .controller('ForecastsCtrl', function ($scope, $state, ForecastList) {
+    $scope.forecastList = ForecastList.all();
 
-  $scope.alert = function(){
-    $state.transitionTo('app.alert');
-  };
+    $scope.alert = function () {
+      $state.transitionTo('app.alert');
+    };
 
-  $scope.search = function(){
-    $state.transitionTo('app.search');
-  };
-	
-	$scope.expand = function(id) {
-		var expandItem = jQuery(".forecast-list [data-forecast-id=" + id + "]");
-		if(expandItem.hasClass("opened")) {
-			expandItem.removeClass("opened");
-		} else {
-      jQuery(".forecast-list .forecast-item").removeClass("opened");
-			// expandItem.parents(".scroll").animate({
-			// 	scrollTop: expandItem.offset().top
-			// }, 1000);
-			//expandItem.parents("div.scroll").scrollDiv.css("-webkit-transform", "translated3d(0px, -" + expandItem.position().top + "px, 0px) scale(1)");
-			expandItem.addClass("opened");
-			if(!expandItem.hasClass("data-loaded")) {
-				$scope.forecastList[id].detail = ForecastList.getForecastDetail(id);
-				expandItem.addClass("data-loaded");
-			}			
-		} 
-	};
-})
+    $scope.search = function () {
+      $state.transitionTo('app.search');
+    };
 
-.controller('AlertCtrl', function($scope, $state){
-})
+    $scope.expand = function (id) {
+      var expandItem = jQuery(".forecast-list [data-forecast-id=" + id + "]");
+      if (expandItem.hasClass("opened")) {
+        expandItem.removeClass("opened");
+      } else {
+        jQuery(".forecast-list .forecast-item").removeClass("opened");
+        // expandItem.parents(".scroll").animate({
+        // 	scrollTop: expandItem.offset().top
+        // }, 1000);
+        //expandItem.parents("div.scroll").scrollDiv.css("-webkit-transform", "translated3d(0px, -" + expandItem.position().top + "px, 0px) scale(1)");
+        expandItem.addClass("opened");
+        if (!expandItem.hasClass("data-loaded")) {
+          $scope.forecastList[id].detail = ForecastList.getForecastDetail(id);
+          expandItem.addClass("data-loaded");
+        }
+      }
+    };
+  })
+
+  .controller('AlertCtrl', function ($scope, $state) {
+  })
 
 
