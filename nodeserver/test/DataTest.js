9657d8665094b92038c3e4ae5b78bdb8b07bcc8e
Daryl Zhang <tiden111@gmail.com>
2014-11-10 10:44:04 +1030

    data service

---
+++ nodeserver/test/DataTest.js
@@ -0,0 +1,15 @@
+var request = require('supertest');
+var assert = require('assert');
+var connection = require('mongoose').connection;
+var debug = require('debug')('dev');
+
+var models = require('../models');
+var app = require('../app.js');
+var DataService = require('../DataService');
+
+describe('Data Service Test', function() {
+
+
+});
+
+describe('Data Route Test', function() {});
