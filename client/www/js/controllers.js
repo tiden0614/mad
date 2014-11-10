aca5785f0d3c11386e409b728eb3277c57e170ef
cl <zhouqihang@gmail.com>
2014-11-10 10:43:32 +1030

    Weather JSON

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -130,54 +130,92 @@ angular.module('farmers.controllers', [])
 })
 
 .controller('ForecastDetailCtrl', function ($scope, $stateParams, ForecastList, TempHourlyList, RainfallThreeHourlyList){
-    $scope.forecast = ForecastList.getForecastDetail();
+    var detailURL="templates/weather.json";
+    var windSpeed = [{"key": "Wind", "values": []}];
+    var tempDaily = [];
+    jQuery.getJSON(detailURL, function(data){
+
+    for (var i=0; i<24; i++){
+        var windArray = [new Date (0,0,0,i,0,0), data.windSpeed[i]];
+        windSpeed[0].values.push(windArray);
+
+        var tempObj = {hour: i, temp_hourly: data.temp[i]};
+        tempDaily.push(tempObj);
+
+    }
+    //alert([0].values.length);
+    $scope.windSpeed=windSpeed;
+
+    $scope.temps_hourly=[];
+
+                     // need to change if updated
+                     //      var tempDetail_width = $(window).width();
+    var tempDetail_width = 998;
+    var tempDetail_height = 135;
+
+    function getMaxTemp(tempArray){
+        var temp = tempArray[0].temp_hourly;
+        for(i in tempArray){
+            if( tempArray[i].temp_hourly>temp)
+            {
+            temp = tempArray[i].temp_hourly;
+            }
+        }
+        return temp;
+    };
+
+    function getMinTemp(tempArray){
+        var temp = tempArray[0].temp_hourly;
+        for(i in tempArray){
+            if( tempArray[i].temp_hourly<temp)
+            {
+            temp = tempArray[i].temp_hourly;
+            }
+        }
+        return temp;
+    };
+
+    var tempMax = getMaxTemp(tempDaily);
+    var tempMin = getMinTemp(tempDaily);
+
+    for ( i in tempDaily) {
+        if (tempDaily[i].hour<13){
+            $scope.temps_hourly.push({
+            x : i * tempDetail_width/( tempDaily.length + 1)+10,
+            y : (tempDetail_height*0.4/( tempMax - tempMin + 1)) * ( tempMax - tempDaily[i].temp_hourly)+20 ,
+            v:  tempDaily[i].temp_hourly+'\u00b0C',
+            t:  tempDaily[i].hour+'AM'
+            });
+        }else{
+            $scope.temps_hourly.push({
+            x : i * tempDetail_width/( tempDaily.length + 1)+10,
+            y : (tempDetail_height*0.4/( tempMax - tempMin + 1)) * ( tempMax - tempDaily[i].temp_hourly)+20 ,
+            v:  tempDaily[i].temp_hourly+'\u00b0C',
+            t:  tempDaily[i].hour+'PM'
+            })
+        }
+    }
+
+    });
+
+
+    $scope.xAxisTickFormat = function(){
+        return function(d){
+            return d3.time.format('%H')(new Date(d));
+        }
+    }
+
+    $scope.xAxisTickValues = function(){
+        return function(d){
+            var tickVals = [];
+            var values = d[0].values;
+            for (var i in values){
+                tickVals.push(values[i][0]);
+            }
+            return tickVals;
+        }
+    }
 
     $scope.rainfallList = RainfallThreeHourlyList.all();
 
-                  $scope.temps_hourly=[];
-                  var tempDaily = TempHourlyList.all();
-
-                 // need to change if updated
-                 //      var tempDetail_width = $(window).width();
-                  var tempDetail_width = 998;
-                  var tempDetail_height = 135;
-
-                  var tempMax = TempHourlyList.getMaxTemp();
-                  var tempMin = TempHourlyList.getMinTemp();
-
-                    for ( i in tempDaily) {
-                                                    if (tempDaily[i].hour<13){
-                                                      $scope.temps_hourly.push({
-                                                            x : i * tempDetail_width/( tempDaily.length + 1)+10,
-                                                            y : (tempDetail_height*0.4/( tempMax - tempMin + 1)) * ( tempMax - tempDaily[i].temp_hourly)+20 ,
-                                                            v:  tempDaily[i].temp_hourly+'\u00b0C',
-                                                            t:  tempDaily[i].hour+'AM'
-                                                          });
-                                                      }else{
-                                                       $scope.temps_hourly.push({
-                                                                             x : i * tempDetail_width/( tempDaily.length + 1)+10,
-                                                                             y : (tempDetail_height*0.4/( tempMax - tempMin + 1)) * ( tempMax - tempDaily[i].temp_hourly)+20 ,
-                                                                             v:  tempDaily[i].temp_hourly+'\u00b0C',
-                                                                             t:  tempDaily[i].hour+'PM'
-                                                                            })
-                                                      }
-                                           }
-
-            $scope.xAxisTickFormat = function(){
-                return function(d){
-                    return d3.time.format('%H')(new Date(d));
-                }
-            }
-
-            $scope.xAxisTickValues = function(){
-                return function(d){
-                    var tickVals = [];
-                    var values = d[0].values;
-                    for (var i in values){
-                        tickVals.push(values[i][0]);
-                    }
-                    console.log('xAxisTickValuesFunction', d);
-                    return tickVals;
-                }
-            }
 })
