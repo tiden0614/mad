778d7f9e5b3250140c3c0f957ca56827bc6ac401
Daryl Zhang <tiden111@gmail.com>
2014-11-12 17:01:15 +1030

    notification data

--- nodeserver/services/mock/notification-forecast.js
+++ nodeserver/services/mock/notification-forecast.js
@@ -1,8 +1,8 @@
 module.exports = {
   "baseTime": "2014/11/8  06:00:00",
-  "fog": [{"code":"5"},{"code":"2"},{"code":"3"},{"code":"12"},{"code":"6"},{"code":"0"},{"code":"0"},{"code":"5"},{"code":"8"},{"code":"0"},{"code":"22"},{"code":"26"},{"code":"18"},{"code":"3"},{"code":"0"},{"code":"0"}],
-  "frost": [{"code":"5"},{"code":"2"},{"code":"3"},{"code":"12"},{"code":"6"},{"code":"0"},{"code":"0"},{"code":"5"},{"code":"8"},{"code":"0"},{"code":"22"},{"code":"26"},{"code":"18"},{"code":"3"},{"code":"0"},{"code":"0"}],
+  "fog": [{"code":"0"},{"code":"2"},{"code":"3"},{"code":"0"},{"code":"0"},{"code":"0"},{"code":"0"},{"code":"0"},{"code":"0"},{"code":"0"},{"code":"0"},{"code":"0"},{"code":"1"},{"code":"3"},{"code":"0"},{"code":"0"}],
+  "frost": [{"code":"5"},{"code":"2"},{"code":"3"},{"code":"0"},{"code":"0"},{"code":"0"},{"code":"0"},{"code":"0"},{"code":"0"},{"code":"0"},{"code":"0"},{"code":"0"},{"code":"18"},{"code":"3"},{"code":"0"},{"code":"0"}],
   "precipitation": [{"code":"5"},{"code":"2"},{"code":"3"},{"code":"12"},{"code":"6"},{"code":"0"},{"code":"0"},{"code":"5"},{"code":"8"},{"code":"0"},{"code":"22"},{"code":"26"},{"code":"18"},{"code":"3"},{"code":"0"},{"code":"0"}],
-  "fronzenPrec": [{"code":"5"},{"code":"2"},{"code":"3"},{"code":"12"},{"code":"6"},{"code":"0"},{"code":"0"},{"code":"5"},{"code":"8"},{"code":"0"},{"code":"22"},{"code":"26"},{"code":"18"},{"code":"3"},{"code":"0"},{"code":"0"}],
+  "frozenPrec": [{"code":"5"},{"code":"2"},{"code":"3"},{"code":"12"},{"code":"6"},{"code":"0"},{"code":"0"},{"code":"5"},{"code":"8"},{"code":"0"},{"code":"22"},{"code":"26"},{"code":"18"},{"code":"3"},{"code":"0"},{"code":"0"}],
   "thunderstorm": [{"code":"5"},{"code":"2"},{"code":"3"},{"code":"12"},{"code":"6"},{"code":"0"},{"code":"0"},{"code":"5"},{"code":"8"},{"code":"0"},{"code":"22"},{"code":"26"},{"code":"18"},{"code":"3"},{"code":"0"},{"code":"0"}]
 };
